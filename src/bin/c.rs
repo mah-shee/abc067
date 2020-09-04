cbddfb44c2c7b89208c56f8f0aa15fbb6b3f61a9
masa <masatoshi.sboard@gmail.com>
2020-09-05 06:37:43 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,8 +1,25 @@
 #[allow(unused_imports)]
 use proconio::marker::{Bytes, Chars, Usize1};
 use proconio::{fastout, input};
+use std::cmp::min;
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        a: [isize; n],
     }
+    let mut ans = std::isize::MAX;
+    let mut an = vec![0; n];
+    let mut sum = 0;
+    an[0] = a[0];
+    sum += a[n - 1];
+    for i in 0..n - 1 {
+        an[i + 1] = an[i] + a[i + 1];
+        sum += a[i];
+    }
+    for i in 0..n - 1 {
+        let diff = (sum - 2 * an[i]).abs();
+        ans = min(ans, diff);
+    }
+    println!("{}", ans);
 }
